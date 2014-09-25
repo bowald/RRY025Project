676ecf452d676cf680b30ff9dc79fe1449f5df55
Bowald <j_bowald@hotmail.com>
2014-09-25 11:53:54 +0200

    Added TODO-Comments

--- main.m
+++ main.m
@@ -6,8 +6,7 @@ forest = log(1 + forestgray);
 forest = fft2(forest);
 
 % Build H(u,v)
-type raduv.m
-rforest = raduv(forest);
+rforest = raduv(forest); %helper function for geting the distance from u and v.
 
 % Constants
 cut   = [2:9];
@@ -19,9 +18,13 @@ Yl    = 0.25; % < 1
 results = {};
 for i = 1:length(cut)
     H = (Yh - Yl) .* (1 - exp(-c *(rforest.^2 ./ (cut(i)^2) ) ) ) + Yl;
+
+    %The filtering
     procForest = H.*forest;
+
+    %Reverse transform and save in array
     procForest = ifft2(procForest);
-    procForest = exp(procForest) + 1;
+    procForest = exp(procForest) - 1; %reverse log(x + 1)
     results{1,i} = real(procForest);
 end
 
@@ -32,3 +35,8 @@ displayImageGrid(results,cut);
 
 
 %imshow([forestgray, results{2}],[])
+
+% read 4.7.3
+% Check lecture IMAGE ENHANCMENT IV
+% Zero padding
+% Centering FFTWShift
