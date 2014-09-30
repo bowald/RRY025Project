3772ffbd3a77d09450d9ceada6ef3f701de08e0c
Bowald <j_bowald@hotmail.com>
2014-09-30 15:18:13 +0200

    fixed a distance function, workable code!

--- main.m
+++ main.m
@@ -1,53 +1,58 @@
 %%
-load('forest.mat');
 
-% init functions
-imshow(forestgray)
-% forest = histeq(forestgray);
-% figure
-% imshow(forest,[])
-forest = log(1 + forestgray);
-% med padding skall bilden vara 601 x 893
+% Constants
 n = 301;
 m = 447;
 q = 2*m - 1;
 p = 2*n - 1;
-
-forest = padarray( forest , [p-n q-m],  0, 'post');
-forest = fft2(forest);
-forest = fftshift(forest);
-
-% Build H(u,v)
-rforest = raduv(forest); %helper function for geting the distance from u and v.
-
-% Constants
 cut   = [40:10:80];
 c     = 1;
 Yh    = 2; % > 1
 Yl    = 0.25; % < 1
 
-% % Homomorphic filter
+load('forest.mat');
+imshow(forestgray)
+
+% log
+forest = log(1 + forestgray);                         
+
+% Zero padding
+forest = padarray( forest , [p-n q-m],  0, 'post');   
+
+% Transform
+forest = fft2(forest);                                
+
+% Shift
+forest = fftshift(forest);                            
+  
+  % Setup D(x,y)
+  [u, v] = meshgrid(1:q,1:p);
+  centerU = ceil(q/2);
+  centerV = ceil(p/2);
+
 for i = 1:length(cut)
-    H = (Yh - Yl) .* (1 - exp(-c *(rforest.^2 ./ (cut(i)^2) ) ) ) + Yl;
+  gaussianNumerator = (u - centerU).^2 + (v - centerV).^2;
+  H = 1 - exp(-c * (gaussianNumerator./(cut(i).^2)));
+  H = (Yh - Yl) * H + Yh;
 
     %The filtering
-    procForest = forest.*H;
+    procForest = H.*forest;
+
+    % Shift
     procForest = ifftshift(procForest);
+
+    % I-Transform
     procForest = ifft2(procForest);
+
+    % Crop
     procForest = procForest(1:n, 1:m);
+
+    % Inverse log
     procForest = exp(procForest) - 1; %reverse log(x + 1)
+
+    % Result only in real values
     result = real(procForest);
 
     figure
     imshow(result,[])
 end
-
-% Display images neatly
-% displayImageGrid(results,cut);
-
-%imshow([forestgray, results{2}],[])
-
-% read 4.7.3
-% Check lecture IMAGE ENHANCMENT IV
-% Zero padding
-% Centering FFTWShift
