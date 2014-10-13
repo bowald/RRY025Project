89710fbfa1d52b28cbaa20bff4bb526a16a2c6f7
Anton Johansson <tomaten666@gmail.com>
2014-10-13 10:46:59 +0200

    result

--- main.m
+++ main.m
@@ -1,16 +1,22 @@
 %%
 
 % Constants
+close all;
+cut   = [10 10];  % Grey/tolarance. Higher, a smaler spectra of grey values.
+c     = 1;
+Yh    = [1 1]; % > 1  %Brightness higher is darker
+Yl    = [0.01 0.8]; % < 1 %lower makes black areas more black
 
-cut   = 10;  % Grey/tolarance. Higher, a smaler spectra of grey values.
-% c     = 1;
-Yh    = [1.2]; % > 1  %Brightness higher is darker
-Yl    = [0.2]; % < 1 %lower makes black areas more black
+%forestgray = im2double(imread('AT3_1m4_01.tif'));
+%forestgray = im2double(imread('rice.tif'));
 
-% forestgray = im2double(imread('pout.tif'));
 load('forest.mat');
+
 figure
   imshow(forestgray, []) % Original image
+histi = histeq(forestgray);
+%figure
+%    imshow(histi)
 n = size(forestgray,1);
 m = size(forestgray,2);
 q = 2*m - 1;
@@ -18,8 +24,7 @@ p = 2*n - 1;
 % log
 forest = log(forestgray);
 
-figure
-  % imshow(forest, []); % Logarithm of the image
+% imshow(forest, []); % Logarithm of the image
 
 % Zero padding
 forest = padarray( forest , [p-n q-m],  0, 'post');   
@@ -37,10 +42,11 @@ forest = fftshift(forest);
 
 for i = 1:length(Yl)
   gaussianNumerator = ((u - centerU).^2 + (v - centerV).^2);
-  H = 1 - exp( - (gaussianNumerator./ (2* cut.^2) ) );
+  H = 1 - exp(-c*(gaussianNumerator./ (2* cut(i).^2) ) );
   H = (Yh(i) - Yl(i)) * H + Yl(i);
-  % figure
-  % imshow(H,[]);
+  
+   figure
+   surfc(H,'Edgecolor','none');
     %The filtering
     procForest = H.*forest;
 
