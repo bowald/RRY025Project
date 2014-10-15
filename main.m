4c6abf13bd1539239de738e67d283463688821fe
Bowald <j_bowald@hotmail.com>
2014-10-15 15:09:57 +0200

    updated appendix

--- main.m
+++ main.m
@@ -1,45 +1,41 @@
 %%
 % Constants
 close all;
-cut   = 6;  % Grey/tolarance. Higher, a smaler spectra of grey values.
-c     = 1;
-Yh    = 1; % > 1  %Brightness higher is darker
-Yl    = [0.5]; % < 1 %lower makes black areas more black
+cut   = 6;  % cut-of frequency, steepness and width of the gaussian function.
+Yh    = 1; % maximum enhancement of high frequencies.
+Yl    = 0.5; % maximum inhibition of low frequencies.
 
-%forestgray = (im2double(imread('friend.tif')));
-%forestgray = im2double(imread('rice.tif'));
-load('forest.mat');
-forestgray = forestgray + 0.00001;
-figure
-  imshow(forestgray, []) % Original image
-%histi = histeq(forestgray);
-%figure
+load('forest.mat'); %load input image
+
+% Zero padding constants
 n = size(forestgray,1);
 m = size(forestgray,2);
 q = 2*m - 1;
-p = 2*n - 1;
+p = 2*n - 1; 
+
 % log
 forest = log(forestgray);
 
 % Zero padding
 forest = padarray( forest , [p-n q-m],  0, 'post');   
+
 % Transform
 forest = fft2(forest);                                
 
 % Shift
 forest = fftshift(forest);                            
   
-  % Setup D(x,y)
+  % Setup D(x,y), the distance function
   [u, v] = meshgrid(1:q,1:p);
   centerU = ceil(q/2);
   centerV = ceil(p/2);
 
-for i = 1:length(Yl)
+for i = 1:length(Yl) %cut, yl and yh can be entered as vectors for parameter study.
   gaussianNumerator = ((u - centerU).^2 + (v - centerV).^2);
-  H = 1 - exp(-c*(gaussianNumerator./ (2* cut(i).^2) ) );
+  H = 1 - exp(-(gaussianNumerator./ (2* cut(i).^2) ) ); % The filter
   H = (Yh(i) - Yl(i)) * H + Yl(i);
   
-    %The filtering
+    %Filtering the image
     procForest = H.*forest;
 
     % Shift
@@ -52,7 +48,7 @@ for i = 1:length(Yl)
     procForest = procForest(1:n, 1:m);
 
     % Inverse log
-    procForest = exp(procForest); %reverse log(x + 1)
+    procForest = exp(procForest); %inverse log(x)
 
     % Result only in real values
     result = real(procForest);
